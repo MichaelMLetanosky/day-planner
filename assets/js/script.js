5e92ee144b2d52b7a4cb56ba2b1249b57729883f
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-06 20:16:15 -0400

    Set up hours area html and styling

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//todo Add day
+
+
+
+//todo pull current time
+//Check time to set correct past/present/future colors
+
+//Grab info from form when save button is pressed
+//Set info from form to local.storage
+//display local.storage text onto page
