e54d0f2ecc297b8623ddaa571ab51b6de4fad932
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-06 20:54:39 -0400

    Added function to set color coding of hour blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
-//todo Add day
+//Global Variables
 
+//Displays current day of the week and day of the month
+var now = moment();
+$("#currentDay").text(now.format("dddd, MMMM Do"));
 
+//Check current hour and compare to hour blocks
+function setHour () {
+    for (let i=9; i<18; i++){
+        //Sets class to past if hour is done
+        if (i < now.format("HH")) {
+            $("#hour" + i).addClass("past");
+        //Sets class to future if hour is yet to come
+        } else if (i > now.format("HH")) {
+            $("#hour" + i).addClass("future");
+        //Sets class to present if current hour
+        } else {
+            $("#hour" + i).addClass("present");
+        };
+    };
+};
 
-//todo pull current time
+setHour ();
+
 //Check time to set correct past/present/future colors
 
 //Grab info from form when save button is pressed
